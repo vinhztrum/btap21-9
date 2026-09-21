94d1c715cad13fdf30612634b665ce66f0632124
LAPTOP-D114OISI\vinhy <vinhyhan@gmail.com>
2026-09-21 13:52:26 +0700

    add binding

--- PhamTranThanhVinh/app/src/main/java/com/ute/phamtranthanhvinh/MainActivity.kt
+++ PhamTranThanhVinh/app/src/main/java/com/ute/phamtranthanhvinh/MainActivity.kt
@@ -1,1 +1,17 @@
 package com.ute.phamtranthanhvinh
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.studentprofile.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    // Bước 1: Khai báo biến binding với lateinit var
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+// Bước 2: Nạp layout XML thông qua hàm inflate()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+// Bước 3: Truyền root view vào setContentView
+        setContentView(binding.root)
+// Bước 4: Thao tác View trực tiếp, không lo Null!
+        binding.tvWelcome.text = "Chào mừng bạn đến với ViewBinding!"
+    }
+}
